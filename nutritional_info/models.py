6965ca968c2ee6ac80a9aa47cf5dc615f81ec2bc
Robson J <robsonjrservices@gmail.com>
2023-10-22 11:32:51 -0300

    FEAT: Create models with receipes

--- nutritional_info/models.py
+++ nutritional_info/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class receipes(models.Model):
+    #Models with receipes
+    name_receipe = models.CharField(max_length=200)
+    image = models.ImageField()
+    ingredients = models.TextField(max_length=2000)
+    tutorial = models.TextField(max_length=2000)
+
+    #nutritional info
+    calories = models.FloatField(null=True, blank=True)
+    carbo = models.FloatField(null=True, blank=True)
+    fat = models.FloatField(null=True, blank=True)
+    protein = models.FloatField(null=True, blank=True)
